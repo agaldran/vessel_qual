855f194074e49e0b7ef0cbe95dc4d77d4f74bf36
= <agaldran@gmail.com>
2020-04-18 13:01:56 +0100

    bug

--- train_vessel_qual.py
+++ train_vessel_qual.py
@@ -90,7 +90,7 @@ def run_one_epoch_reg(loader, model, criterion, optimizer=None):
             logits = model(inputs)
             preds = torch.sigmoid(logits)
             loss = criterion(logits.squeeze(), labels)
-            print('This batch 0th logit/pred/label/loss=', logits[0].item(),preds[0].item(),labels[0].item(), loss[0].item())
+            print('This batch 0th logit/pred/label/loss=', logits[0].item(),preds[0].item(),labels[0].item(), loss.item())
 
             if train:  # only in training mode
                 optimizer.zero_grad()
