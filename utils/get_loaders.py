trying to fix rotation on grayscale

utils/get_loaders.py
@@ -1,7 +1,7 @@
 from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader
 from . import paired_transforms_tv04 as p_tr
-
+import torchvision.transforms as tr
 import os.path as osp
 import pandas as pd
 from PIL import Image
@@ -107,7 +107,7 @@ class RegDataset(Dataset):
             if self.transforms is not None:
                 vessels_original, vessels_pred = self.transforms(vessels_original, vessels_pred)
             # thresholding only needed for non-binary images (predictions)
-            threshold = 255 * (random.random() * 0.25 + 0.25)  # random threshold in [0.25,0.5]
+            threshold = 255 * (random.random() * 0.4 + 0.1)  # random threshold in [0.1,0.5]
 
             vessels_pred = np.array(vessels_pred) > threshold
             # we degrade, compute similarity later
@@ -239,7 +239,7 @@ def get_reg_datasets(csv_path_train, csv_path_val, p_manual=0.5, p_nothing=0.1, 
     # geometric transforms
     h_flip = p_tr.RandomHorizontalFlip()
     v_flip = p_tr.RandomVerticalFlip()
-    rotate = p_tr.RandomRotation(degrees=45)
+    rotate = p_tr.RandomRotation(degrees=45, fill=(0,))
     scale = p_tr.RandomAffine(degrees=0, scale=(0.95, 1.20))
     transl = p_tr.RandomAffine(degrees=0, translate=(0.05, 0))
     # either translate, rotate, or scale

utils/paired_transforms_tv04.py
@@ -1055,12 +1055,14 @@ class RandomRotation(object):
         center (2-tuple, optional): Optional center of rotation.
             Origin is the upper left corner.
             Default is the center of the image.
+        fill (3-tuple or int): RGB pixel fill value for area outside the rotated image.
+            If int, it is used for all channels respectively.
 
     .. _filters: https://pillow.readthedocs.io/en/latest/handbook/concepts.html#filters
 
     """
 
-    def __init__(self, degrees, resample=False, resample_tg=False, expand=False, center=None,fill=0): #
+    def __init__(self, degrees, resample=False, resample_tg=False, expand=False, center=None, fill=0): #
         if isinstance(degrees, numbers.Number):
             if degrees < 0:
                 raise ValueError("If degrees is a single number, it must be positive.")
@@ -1074,7 +1076,7 @@ class RandomRotation(object):
         self.resample_tg = resample_tg
         self.expand = expand
         self.center = center
-        self.fill = (0,)
+        self.fill = fill
 
     @staticmethod
     def get_params(degrees):
@@ -1103,6 +1105,7 @@ class RandomRotation(object):
             #     fill=0
             # else:
             #     fill=
+
             return F.rotate(img, angle, self.resample, self.expand, self.center, self.fill), \
                    F.rotate(target, angle, self.resample_tg, self.expand, self.center, self.fill) #
                    # resample = False is by default nearest, appropriate for targets