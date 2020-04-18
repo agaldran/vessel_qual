5fc41f7fb8964e81f1e4b16b5dbdf219782beb84
= <agaldran@gmail.com>
2020-04-18 12:34:20 +0100

    bug

--- train_vessel_qual.py
+++ train_vessel_qual.py
@@ -121,6 +121,10 @@ def train_reg(model, optimizer, train_criterion, val_criterion, train_loader, va
         # validate one epoch, note no optimizer is passed
         with torch.no_grad():
             vl_preds, vl_labels, vl_loss = run_one_epoch_reg(val_loader, model, val_criterion)
+        print(tr_preds, tr_labels)
+        print(vl_preds, vl_labels)
+        import time
+        time.sleep(1)
         tr_err = train_criterion(tr_labels, tr_preds).detach().numpy()
         print('\n')
         vl_err = train_criterion(vl_labels, vl_preds).detach().numpy()
