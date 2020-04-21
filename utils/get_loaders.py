9162d5714426b6902bd80d78dea187097d5b8839
= <agaldran@gmail.com>
2020-04-21 11:56:52 +0100

    guild

--- utils/get_loaders.py
+++ utils/get_loaders.py
@@ -112,7 +112,8 @@ class RegDataset(Dataset):
                 vessels_original, vessels_pred = self.transforms(vessels_original, vessels_pred)
             # thresholding only needed for non-binary images (predictions)
             # threshold = 255 * (random.random() * 0.4 + 0.1)  # random threshold in [0.1,0.5]
-            threshold = int(255 * 0.5*random.random())  # random threshold in [0.0, 0.5]
+            # threshold = int(255 * 0.5 * random.random())  # random threshold in [0.0, 0.5] # works worse
+            threshold = 255 * (random.random() * 0.3)  # random threshold in [0.0,0.3]
             vessels_pred = np.array(vessels_pred) > threshold
             # we degrade, compute similarity later
             vessels_deg = self.degrade_im(vessels_pred, self.max_deg_patches, self.max_patch_size)
